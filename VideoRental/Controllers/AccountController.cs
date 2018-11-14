ce2d527749fd4719252a99489c56b153b2a14dd9
bZybert <b.zybert@gmail.com>
2018-11-14 14:43:09 +0100

    added login and registred methods

--- VideoRental/Controllers/AccountController.cs
+++ VideoRental/Controllers/AccountController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using VideoRental.Models;
 
@@ -9,6 +11,15 @@ namespace VideoRental.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -16,9 +27,51 @@ namespace VideoRental.Controllers
         }
 
         [HttpPost]
-        public IActionResult Register(AccountViewModels accountViewModel)
+        public async Task<IActionResult> Register(AccountViewModels accountViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                var email = new IdentityUser() { UserName = accountViewModel.Email };
+                var result = await _userManager.CreateAsync(email, accountViewModel.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(accountViewModel);
+        }
+        [HttpGet]
+        public IActionResult LogIn()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogIn(AccountViewModels viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(viewModel.Email,
+                viewModel.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Nie można się zalogować!");
+                }
+            }
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
